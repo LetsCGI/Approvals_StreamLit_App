7cea98e9f1071e7c9d42ed4ce6eb51d1a7e720be
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 14:10:48 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -12,4 +12,47 @@ secondaryBackgroundColor="#F0F2F6"
 textColor="#262730"
 font="sans serif"
 
-st.image("C:\GitHub StreamLit Repository\Approvals_StreamLit_App/BankingAnalyticsLogo.png", use_column_width=True)
+
+
+# button
+st.button('Streamlit Button', help="Click here")
+ 
+# check box
+st.checkbox('Check Box')
+ 
+# radio button
+lang = st.radio(
+    "What's your favorite programming language?",
+    ('C++', 'Python'))
+if lang == 'C++':
+    st.write('You selected C++.')
+else:
+    st.write('You selected Python')
+ 
+# slider
+score = st.slider('Please specify your test score', 0, 100, 10)
+st.write("My test score is ", score)
+ 
+# Using object notation
+add_selectbox = st.sidebar.radio(
+    "Please choose an option",
+    ("Option 1", "Option 2", "Option 3")
+)
+
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('C:\GitHub StreamLit Repository\Approvals_StreamLit_App/BankingAnalyticsLogo.png')    
+    
