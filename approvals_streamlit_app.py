466ff14ebe585f68391befd8699997b4ed5d340e
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 11:49:43 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit 
 streamlit.title('Data Classification Approvals App')
+
+class myProgram:
+    global container
+    global colors
+    global i
+   
    def __init__(self, root):
         self.container = root
         self.colors = ["blue", "yellow", "red", "black", "purple", "white"]
