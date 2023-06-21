ad82b0c8c73ee07fcb2b100c15eebc59a8572c2e
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 11:54:22 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -1,13 +1,4 @@
 import streamlit 
 # importing package
-import turtle
 streamlit.title('Data Classification Approvals App')
 
-
-  
-# set the background color
-# of the turtle screen
-turtle.Screen().bgcolor(0,0,255)
-  
-# move turtle
-turtle.forward(100)
