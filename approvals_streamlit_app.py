cc7a1ae93f249af221e36fb499694b7771067772
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 13:32:48 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit as st
 # importing package
 st.title('Data Classification Approvals App')
 
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 # button
 st.button('Streamlit Button', help="Click here")
  
