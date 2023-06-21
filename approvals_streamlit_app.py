2e9f1504497e4d92791e69531ef9b5278fa733cb
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 14:01:23 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -4,13 +4,15 @@ import streamlit as st
 # importing package
 st.title('Data Classification Approvals App')
 
-st.image("C:\GitHub StreamLit Repository\Approvals_StreamLit_App/BankingAnalyticsLogo.png", use_column_width=True)
+
 
 primaryColor="#F63366"
 backgroundColor="#FFFFFF"
 secondaryBackgroundColor="#F0F2F6"
 textColor="#262730"
 font="sans serif"
+
+st.image("C:\GitHub StreamLit Repository\Approvals_StreamLit_App/BankingAnalyticsogo.png", use_column_width=True)
 
 # button
 st.button('Streamlit Button', help="Click here")
