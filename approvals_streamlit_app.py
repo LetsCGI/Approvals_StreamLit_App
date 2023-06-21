cfc25269f2ebd93817049b2493324b26448e522a
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 11:52:51 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -1,37 +1,13 @@
 import streamlit 
-
-import tkinter as tk
+# importing package
+import turtle
 streamlit.title('Data Classification Approvals App')
 
 
-class myProgram:
-    global container
-    global colors
-    global i
-
-    def __init__(self, root):
-        self.container = root
-        self.colors = ["blue", "yellow", "red", "black", "purple", "white"]
-        self.i = 0
-        self.createView()
-
-    def createView(self):
-        self.container.title("JProgram - Events Clicks")
-        self.container.geometry("640x480+250+100")
-        self.container["bg"] = self.colors[0]
-
-        self.button = tk.Button(self.container, text="Click", background="green", command = self.changeBackground)
-        self.button.place(x=100, y=90)
-
-        self.container.mainloop()
-
-    def changeBackground(self):
-        if(self.container["bg"] == "white"):
-            self.i = -1
-
-        self.i = self.i + 1
-        self.container["bg"] = self.colors[self.i]
-
-if __name__ == '__main__':
-    window = tk.Tk()
-    ap = myProgram(window)
+  
+# set the background color
+# of the turtle screen
+turtle.Screen().bgcolor(0,0,255)
+  
+# move turtle
+turtle.forward(100)
