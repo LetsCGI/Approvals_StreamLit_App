065df2f7c0c1e8c8a4887a6dd4604b614cdb2e91
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 13:33:04 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 # importing package
 st.title('Data Classification Approvals App')
 
-[theme]
+
 primaryColor="#F63366"
 backgroundColor="#FFFFFF"
 secondaryBackgroundColor="#F0F2F6"
