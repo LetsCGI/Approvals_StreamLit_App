190183032e2257a35e466f8e8cd6567e6419fe20
LetsCGI <135914305+LetsCGI@users.noreply.github.com>
2023-06-21 14:11:30 -0500

    Update approvals_streamlit_app.py

--- approvals_streamlit_app.py
+++ approvals_streamlit_app.py
@@ -39,20 +39,3 @@ add_selectbox = st.sidebar.radio(
     ("Option 1", "Option 2", "Option 3")
 )
 
-import base64
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('C:\GitHub StreamLit Repository\Approvals_StreamLit_App/BankingAnalyticsLogo.png')    
-    
